ee8ff0325e46e2f2e4e62b652923b02d9a416513
iamme4146 <fluke412546@gmail.com>
2023-01-07 09:52:02 +0700

    new many file

---
+++ banner.php
@@ -0,0 +1,4 @@
+<div class = "p-5 bg-danger text-white text-center">
+  <h1>30901-2111-65 Day 5</h1>
+  <p>Resize this responsive page to see the effect!</p> 
+</div>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include 'header.php' ?>
+<?php include 'banner.php' ?>
+<?php include 'navigator.php' ?>
+<?php include 'connect.php' ?>
+<?php include 'footer.php' ?>

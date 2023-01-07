98edd5c767b23ffdfaef215955dacb20cdb596ba
iamme4146 <fluke412546@gmail.com>
2023-01-07 10:28:25 +0700

    edit index

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
-include "header.php";
-include "banner.php";
-include "navigator.php";
-include "connect.php";
+    include "header.php";
+    include "banner.php";
+    include "navigator.php";
+    include "connect.php";
 ?>
-<div class = "container mt - 5">
-    <h1>Page Index</h1>
-</div>
-<?php include "footer.php"; ?>
+    <div class = "container mt - 5">
+        <h1>Page Index</h1>
+    </div>
+<?php 
+    include "footer.php"; 
+?>
